1940bbb8db9c324e9d13043f0043e1132ac0c14d
Code Nation <codenation@Codes-MacBook-Pro.local>
2019-12-06 15:26:01 +0000

    notes added

--- src/App.js
+++ src/App.js
@@ -3,9 +3,12 @@ import './App.css';
 
 class App extends React.Component{
 
+  //---- First create the intital state ----//
   state = {
     clicks:0,
   }
+
+  //---- Create a function that causes the state to change with set state ----//
   incrementItem = () => {
     this.setState ({clicks: this.state.clicks + 1})
   }
@@ -15,6 +18,8 @@ class App extends React.Component{
 
   render(){
     return(
+
+      // return the information in via HTML
       <div>
       <h1>{this.state.clicks}</h1>
         <button onClick={this.incrementItem}>Increment</button>
